1578fd022140e448c1c30c1a42ccd216b7692687
necogamy <madrazonecogamy@gmail.com>
2021-08-22 19:00:50 -0300

    Done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  describe('div.App', () => {
+      test('should render 0', () => {
+        // Setup
+        const wrapper = shallow(<App />);
+        let counter = parseInt(wrapper.find('div > h1').text());        
+  
+        // Verify
+        expect(counter).toBe(0);
+      });
+    
+      test('when the button is clicked it should add 1 to the counter and render it', () => {
+        // Setup
+        const wrapper = shallow(<App />);
+        const button = wrapper.find('div > button');
+        let counter;
+  
+        // Exercise
+        button.simulate('click');
+        counter = parseInt(wrapper.find('div > h1').text());
+  
+        // Verify
+        expect(counter).toBe(1);
+      });
+  
+      test('when the button is three times clicked it should add 1 to the counter and render it', () => {
+        const wrapper = shallow(<App />);
+        const button = wrapper.find('div > button');
+        let counter;
+  
+        for (let i = 0; i < 3; i++) button.simulate('click');
+        counter = parseInt(wrapper.find('div > h1').text());
+  
+        expect(counter).toBe(3);
+      });
+
+    
+    });
+
+
 });
